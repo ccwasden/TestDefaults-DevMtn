881f8253ff41a44d39f56e759608100249d1b633
Chase Wasden <chase@jane.com>
2015-01-14 18:06:36 -0700

    Initial Commit

---
+++ TestDefaults/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDefaults
+//
+//  Created by Chase Wasden on 1/14/15.
+//  Copyright (c) 2015 Chase Wasden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
